b9ab752fdd6e188f778115c026879d1cb1684ea9
alphegasolutions <alphegasolutions@gmail.com>
2023-01-18 08:53:28 -0500

    service update

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,21 +1,14 @@
-import { User } from "../model/model";
-import { RoleType } from "../model/zbi.enum";
+import { UserMongoRepository } from "../repositories/mongodb/user.mongo.repository";
+import { IAMRepository } from "../repositories/repository.interface";
 
 export default class UserService {
 
-    async findUsers() {
+    private iamRepository: IAMRepository|undefined;
 
+    constructor() {
     }
 
-    async findUser(userName: string) {
-
-    }
-
-    async findUserByEmail(email: string) {
-
-    }
-
-    async createUser(userName: string, email: string, name: string, role: RoleType) {
-
+    setUserRepository(repo: IAMRepository) {
+        this.iamRepository = repo;
     }
 }
