85cfe0920a2a3cb3b6fa454516d934accd905974
alphegasolutions <alphegasolutions@gmail.com>
2023-01-24 02:35:26 -0500

    updated logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,25 +1,28 @@
 import {Request, Response, NextFunction} from 'express';
-import { createLogger, format, transports } from "winston";
+import { createLogger, format, transports, addColors } from "winston";
 import context from "./context";
-import uuid from 'uuid';
+import {v4 as uuidv4} from 'uuid';
+import morgan, {StreamOptions} from "morgan";
 
 const { combine, label, timestamp, printf, json, colorize } = format;
 
+
+addColors({error: 'red', warn: 'yellow', info: 'green', http: 'magenta', debug: 'white'});
 const console = new transports.Console();
 
-const logFormat = printf(({level, message, label: logLabel, timestamp: logTimeStamp, requestId}) => {
-    return `${level} | ${logTimeStamp} | ${requestId} | ${message}`;
+const logFormat = printf(({level, message, label: logLabel, timestamp: logTimeStamp, requestId, user, service}) => {
+    return `${level} | ${logTimeStamp} | ${requestId} | ${user} | ${service} | ${message}`;
 });
 
 export const defaultLogger = createLogger({
-    level: process.env.LOG_LEVEL || 'info',
+    level: process.env.LOG_LEVEL || 'debug',
     format: combine(label({label: process.env.NODE_ENV}), timestamp(), logFormat, colorize({all: true})),
     transports: [console],
 });
 
-//export default logger;
+export const morganStream: StreamOptions = {write: (message: string) => logger.http(message)};
 
-export const logger = new Proxy(defaultLogger.child({requestId: "zbi"}), {
+export const logger = new Proxy(defaultLogger.child({requestId: uuidv4(), user: "zbi", service: "main"}), {
     get(target, property, receiver) {
       target = context.getStore()?.get('logger') || target;
       return Reflect.get(target, property, receiver);
@@ -27,7 +30,7 @@ export const logger = new Proxy(defaultLogger.child({requestId: "zbi"}), {
 });
 
 module.exports.contextMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const child = logger.child({ requestId: uuid.v4() });
+    const child = logger.child({ requestId: uuidv4() });
     const store = new Map();
   
     return context.run(store, () => {
