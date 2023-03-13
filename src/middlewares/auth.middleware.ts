95b8cdbe6c076b4da6ada84c558e6f6b22ce2e6d
alphegasolutions <alphegasolutions@gmail.com>
2023-03-13 17:11:23 -0400

    set verifier middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { auth } from 'express-oauth2-jwt-bearer';
 import { mainLogger as logger } from '../libs/logger';
-
+import beanFactory from '../factory/bean.factory';
 
 const TENANT_ID = process.env.AUTH0_TENANT_ID;
 const DOMAIN = process.env.AUTH0_DOMAIN;
@@ -9,17 +9,19 @@ const CLIENT_ID = process.env.AUTH0_CLIENT_ID
 const CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET
 const AUDIENCE = process.env.AUTH0_AUDIENCE
 
-const auth0jwtVerifier = auth({
-    audience: `${AUDIENCE}`,
-    issuerBaseURL: `https://${TENANT_ID}.${DOMAIN}/`,
-    tokenSigningAlg: 'RS256',
-    jwksUri: `https://${TENANT_ID}.${DOMAIN}/.well-known/jwks.json`
-});
+export const jwtVerifier = beanFactory.getIdentityService().getAccessVerifier();
+
+// auth({
+//     audience: `${AUDIENCE}`,
+//     issuerBaseURL: `https://${TENANT_ID}.${DOMAIN}/`,
+//     tokenSigningAlg: 'RS256',
+//     jwksUri: `https://${TENANT_ID}.${DOMAIN}/.well-known/jwks.json`
+// });
 
 const basicJwtVerifier = function(request: Request, response: Response, next: NextFunction) {
     logger.info(`checking for jwt ...`);
     next();
 }
 
-export const jwtVerifier = basicJwtVerifier;
+//export const jwtVerifier = basicJwtVerifier;
 // export const jwtVerifier = auth0jwtVerifier;
