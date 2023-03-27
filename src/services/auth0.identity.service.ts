8dd88c6a2979640bad6fa89decc984d52db872df
alphegasolutions <alphegasolutions@gmail.com>
2023-03-27 02:06:23 -0400

    fixed auth0 authentication

--- src/services/auth0.identity.service.ts
+++ src/services/auth0.identity.service.ts
@@ -1,9 +1,10 @@
 import axios, { HttpStatusCode } from "axios";
-import { User, UserInfo } from "../model/model";
+import { User } from "../model/model";
 import { ServiceError, ServiceErrorType } from "../libs/errors";
 import { IdentityService, UserRepository } from "../interfaces";
 import { Handler } from "express";
 import { auth } from 'express-oauth2-jwt-bearer';
+import { getLogger } from "../libs/logger";
 
 //const AUTH0_TENANT_ID = process.env.AUTH0_TENANT_ID;
 //const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
@@ -30,10 +31,8 @@ export default class Auth0IdentityService implements IdentityService {
     private repository: UserRepository;
 
     constructor(userRepository: UserRepository) {
+        const logger = getLogger("auth0-init");
         this.repository = userRepository;
-        this.getToken();
-        this.expiration = 3600;
-
         this.getToken();
     }
 
@@ -262,14 +261,20 @@ export default class Auth0IdentityService implements IdentityService {
     }
 
     private async getToken() {
+        const logger = getLogger("token");
         try {
             const data = {
                 client_id: CLIENT_ID, client_secret: CLIENT_SECRET, 
-                audience: `${AUTH0_URL}/api/v2`, grant_type: 'client_credentials'
+                audience: `${AUDIENCE}`, grant_type: 'client_credentials'
             };
             const response = await axios.post(`${AUTH0_URL}/oauth/token`, JSON.stringify(data), {headers: {"content-type": "application/json"}});
-            console.log(`Response: ${response}`);
+            logger.debug(`Response: ${JSON.stringify(response.data)}`);
             this.token = response.data.access_token;
+            this.expiration = response.data.expires_in;
+
+            logger.info(`token = ${this.token}`);
+            logger.info(`expiration = ${this.expiration}`);
+    
         } catch(err) {
             throw err;
         }
